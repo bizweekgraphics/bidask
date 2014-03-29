4ee8abe8e51aa5ac891c651a857c86dcb172aebb
Toph Tucker <tophtucker@gmail.com>
2014-03-28 20:37:44 -0400

    Revert 694b6df..aacb9aa
    This rolls back to commit 694b6df1daf2974921facd9bd491728410ccb0b0.

--- js/main.js
+++ js/main.js
@@ -22,17 +22,10 @@ var king = new commodity();
 var offerInterval = setInterval(addNewOffer, 1000/king.volume)
 
 function addNewOffer() {
-  if($(".offer").length > 10) {
-    clearInterval(offerInterval);
-    return;
-  }
-
   var newOffer = new offer(king);
+
   offerTemplate = $("#offer-template").html();
-  var appendMe = $(_.template(offerTemplate,{d:newOffer}));
-  appendMe.appendTo("#order-book");
-  appendMe.find(".ask").drags();
-  appendMe.find(".offer").on("mouseenter", function() { $(this).css("border", "1px solid magenta"); });
+  $(_.template(offerTemplate,{d:newOffer})).appendTo("#order-book").on("click", takeOffer);
 
 }
 
@@ -272,48 +265,3 @@ function getDateFromExcel(excelDate) {
   // 2. Convert to milliseconds.
 	return new Date((excelDate - (25567 + 1))*86400*1000);
 }
-
-// Make things draggable
-// @author http://css-tricks.com/snippets/jquery/draggable-without-jquery-ui/
-(function($) {
-    $.fn.drags = function(opt) {
-
-        opt = $.extend({handle:"",cursor:"move"}, opt);
-
-        if(opt.handle === "") {
-            var $el = this;
-        } else {
-            var $el = this.find(opt.handle);
-        }
-
-        return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
-            if(opt.handle === "") {
-                var $drag = $(this).addClass('draggable');
-            } else {
-                var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
-            }
-            var z_idx = $drag.css('z-index'),
-                drg_h = $drag.outerHeight(),
-                drg_w = $drag.outerWidth(),
-                pos_y = $drag.offset().top + drg_h - e.pageY,
-                pos_x = $drag.offset().left + drg_w - e.pageX;
-            $drag.css('z-index', 1000).parents().on("mousemove", function(e) {
-                $('.draggable').offset({
-                    top:e.pageY + pos_y - drg_h,
-                    left:e.pageX + pos_x - drg_w
-                }).on("mouseup", function() {
-                    $(this).removeClass('draggable').css('z-index', z_idx);
-                });
-            });
-            e.preventDefault(); // disable selection
-        }).on("mouseup", function() {
-            if(opt.handle === "") {
-                $(this).removeClass('draggable');
-            } else {
-                $(this).removeClass('active-handle').parent().removeClass('draggable');
-            }
-            $(this).css({top: 0, left: 0});
-        });
-
-    }
-})(jQuery);
